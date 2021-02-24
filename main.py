60ca65526832f96a9553f4aba9989bc6ac1d4d3d
flowy-data <gayletan@gmail.com>
2021-02-24 16:55:41 +0800

    add uvicorn to run from main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI 
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World" }
+
+@app.get("/info")
+async def info():
+    return {"Info": "placeholder" }    
+
+@app.get("/predict")
+async def predict():
+    return {"prediction": "Test" }    
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port = 8000)
