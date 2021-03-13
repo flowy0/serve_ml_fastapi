fa080780d9997bbbe11c631d006083b19122f633
flowy-data <gayletan@gmail.com>
2021-03-13 10:50:35 +0800

    update based on tutorial and new run.sh script

--- README.md
+++ README.md
@@ -7,16 +7,27 @@
 ```
 conda create -n fastapi python=3.8
 conda activate fastapi
-pip install fastapi
- pip install uvicorn
+pip install -r requirements.txt
 ```
 
+#### Run FastAPI
 Run the App in your command line
 ```
 uvicorn main:app --reload
 ```
-or 
 
+or 
 ```
 python main.py
 ```
+
+or
+run the script `./run.sh`
+
+<br>
+
+#### Access the web interface
+View the API via your browser at 
+- http://127.0.0.1:8000/docs
+  or 
+- http://127.0.0.1:8000/redoc
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from typing import Optional, List
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+class Food(BaseModel):
+    name: str
+    ingredients: List[str] = []
+
+
+@app.post("/food")
+async def prepare_food(food: Food, delivery: bool = False):
+    return {"message": f"Preparing {food.name}", "delivery": f"{delivery}"}
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World" }
+    return {"message": "Hello World"}
+
 
 @app.get("/info")
 async def info():
-    return {"Info": "placeholder" }    
+    return {"Info": "placeholder"}
+
 
 @app.get("/predict")
 async def predict():
-    return {"prediction": "Test" }    
+    return {"prediction": "Test"}
 
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port = 8000)
+    uvicorn.run("main.app", host="127.0.0.1", port=8000)
