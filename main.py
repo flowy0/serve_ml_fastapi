7e639d1fef43ccc56380450dc56b908880a1dfb1
flowy-data <gayletan@gmail.com>
2021-03-13 11:07:44 +0800

    add new api endpoint

--- main.py
+++ main.py
@@ -17,9 +17,22 @@ class Food(BaseModel):
     ingredients: List[str] = []
 
 
-@app.post("/food")
-async def prepare_food(food: Food, delivery: bool = False):
+@app.post("/food_summary")
+async def prepare_food(food: Food):
+    return {"message": f"Preparing {food.name}"}
+
+
+@app.post("/food_delivery")
+async def get_food_delivery(food: Food, delivery: bool = False):
     return {"message": f"Preparing {food.name}", "delivery": f"{delivery}"}
+
+
+@app.post("/food_ingredients")
+async def get_food_ingredients(food: Food):
+    all_ingredients = []
+    for i in food.ingredients:
+        all_ingredients.append(i.lower())
+    return {"message": f"Preparing {food.name}", "ingredients": f"{all_ingredients}"}
 
 
 @app.get("/")
@@ -29,7 +42,7 @@ async def root():
 
 @app.get("/info")
 async def info():
-    return {"Info": "placeholder"}
+    return {"Info": "This is a test project to see how to create API Endpoints"}
 
 
 @app.get("/predict")
